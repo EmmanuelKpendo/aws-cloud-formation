1fd5afc44bf5bf04478472c5bef5934e5bb1e937
Emmanuel Kpendo <emmanuel.kpendo@amalitech.com>
2025-06-02 06:57:01 +0000

    feature: update template

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func handler(ctx context.Context, event events.CloudWatchEvent) (string, error) 
 
 	//Initialize SSM Client
 	ssmClient := ssm.NewFromConfig(cfg)
-	emailParam := fmt.Sprintf("lab-2-user-email-%s", userName)
+	emailParam := fmt.Sprintf("cf-user-%s", userName)
 
 	//Get Email from Parameter Store
 	emailOutput, err := ssmClient.GetParameter(ctx, &ssm.GetParameterInput{
